16b04fee1b59af343b3c91876b2db1ce7650c351
nitesh <nitpa.bh@gmail.com>
2023-10-12 11:45:41 +0530

    first commit

---
+++ ExploreCharts/ExploreChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreChartsApp.swift
+//  ExploreCharts
+//
+//  Created by Nitesh on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploreChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
