012a1412da06a0772342742c7c942c3df45b7b51
nitesh <nitpa.bh@gmail.com>
2023-10-12 11:49:08 +0530

    few changes

--- ExploreCharts/ContentView.swift
+++ ExploreCharts/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "bell")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Ring the bell")
         }
         .padding()
     }
