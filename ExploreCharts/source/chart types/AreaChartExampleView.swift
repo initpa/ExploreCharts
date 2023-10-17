be27e77dec63e5e59b2c2724b89a27b4f81251a0
nitesh <nitpa.bh@gmail.com>
2023-10-17 11:46:14 +0530

    AreaChart

--- ExploreCharts/source/chart types/AreaChartExampleView.swift
+++ ExploreCharts/source/chart types/AreaChartExampleView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import Charts
 
 struct AreaChartExampleView: View {
+    let catData = PetData.catExample
+    let dogData = PetData.dogExamples
+    
+    var data: [(type: String, petData: [PetData])] {
+        [(type: "cat", petData: catData),
+         (type: "dog", petData: dogData)]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(data, id: \.type) { dataSeries in
+            ForEach(dataSeries.petData) { data in
+                AreaMark(x: .value("Year", data.year),
+                         y: .value("Population", data.population))
+            }
+            .foregroundStyle(by: .value("Pet type", dataSeries.type))
+        }
+        .chartXScale(domain: 1998...2024)
+        .aspectRatio(1, contentMode: .fit)
+        .padding()
     }
 }
 
